115f91827ff4da4aa1eb93dc1b7f97a2d9e13dd6
rayymaxx <rayymondodhiambo@gmail.com>
2025-10-16 14:26:45 +0300

    Fix: Changed crew folder

--- marketresearch/src/marketresearch/main.py
+++ marketresearch/src/marketresearch/main.py
@@ -4,7 +4,7 @@ import warnings
 
 from datetime import datetime
 
-from market_research.crew import MarketResearch
+from marketresearch.crew import Marketresearch
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
@@ -23,7 +23,7 @@ def run():
     }
     
     try:
-        MarketResearch().crew().kickoff(inputs=inputs)
+        Marketresearch().crew().kickoff(inputs=inputs)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
@@ -37,7 +37,7 @@ def train():
         'current_year': str(datetime.now().year)
     }
     try:
-        MarketResearch().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+        Marketresearch().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
@@ -47,7 +47,7 @@ def replay():
     Replay the crew execution from a specific task.
     """
     try:
-        MarketResearch().crew().replay(task_id=sys.argv[1])
+        Marketresearch().crew().replay(task_id=sys.argv[1])
 
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
@@ -62,7 +62,7 @@ def test():
     }
     
     try:
-        MarketResearch().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+        Marketresearch().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
